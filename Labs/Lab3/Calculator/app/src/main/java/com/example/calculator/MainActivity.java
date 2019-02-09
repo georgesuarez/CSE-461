74b24bba0e04a3703d05bbb51860db42402f09f2
georgesuarez <georgejsuarez@gmail.com>
2019-02-09 13:17:05 -0800

    Added the calculator app for lab 3

---
+++ Labs/Lab3/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText t1, t2;
+    ImageButton plus, minus, multiply,divide;
+    TextView displayResult;
+    String operator = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        t1 = findViewById(R.id.t1);
+        t2 = findViewById(R.id.t2);
+
+        plus = findViewById(R.id.plus);
+        minus = findViewById(R.id.minus);
+        multiply = findViewById(R.id.multiply);
+        divide = findViewById(R.id.divide);
+
+        displayResult = findViewById(R.id.displayResult);
+
+        // Set listeners
+        plus.setOnClickListener(this);
+        minus.setOnClickListener(this);
+        multiply.setOnClickListener(this);
+        divide.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+       double num1 = 0.0;
+       double num2 = 0.0;
+       double result = 0.0;
+
+       // Check if the fields are empty
+        if (TextUtils.isEmpty(t1.getText().toString()) ||
+                TextUtils.isEmpty(t2.getText().toString())) {
+            return;
+        }
+
+        num1 = Float.parseFloat(t1.getText().toString());
+        num2 = Float.parseFloat(t2.getText().toString());
+
+        // Perform operations
+        // and save the operator for later use
+        switch (v.getId()) {
+            case R.id.plus:
+                operator = "+";
+                result = num1 + num2;
+                break;
+            case R.id.minus:
+                operator = "-";
+                result = num1 - num2;
+                break;
+            case R.id.multiply:
+                operator = "*";
+                result = num1 * num2;
+                break;
+            case R.id.divide:
+                operator = "/";
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+
+        displayResult.setText(num1 + " " + operator + " " + num2 + " = " + result);
+    }
+}
